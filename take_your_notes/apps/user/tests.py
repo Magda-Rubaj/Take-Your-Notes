711f1fa342fb49cbf66eb9228c5ba09496946260
admin <magiru@o2.pl>
2020-05-20 21:48:25 +0200

    unit tests for login

--- take_your_notes/apps/user/tests.py
+++ take_your_notes/apps/user/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from django.urls import reverse_lazy
+from .models import User
 
-# Create your tests here.
+class UserAccessTest(TestCase):
+
+    @classmethod
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username='testuser',
+            email='someemail@mail.com',
+            password='testpassword'
+        )
+
+    def test_non_logged_access(self):
+        response = self.client.get(reverse_lazy('core:home'))
+        self.assertEqual(response.status_code, 302)
+    
+    def test_logged_access(self):
+        self.client.force_login(self.user)
+        response = self.client.get(reverse_lazy('core:home'))
+        self.assertEqual(response.status_code, 200)
+
