1f61ac0af151d707b0eee3ba77554e82cba3e2c4
admin <magiru@o2.pl>
2020-07-25 00:51:18 +0200

    note create tests

--- take_your_notes/apps/note/tests.py
+++ take_your_notes/apps/note/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from django.urls import reverse
+from .models import Note
+from ..core import utils
 
-# Create your tests here.
+
+class NoteTest(TestCase):
+
+    @classmethod
+    def setUp(self):
+        self.user = utils.create_user()
+        self.category = utils.create_category()
+
+    def test_create(self):
+        self.client.force_login(self.user)
+        form_data = {'name':'foo', 'content':'dummy content', 'category':self.category.pk}
+        response = self.client.post('/notes/'+str(self.category.pk)+'/create/', form_data)
+        self.assertRedirects(response, reverse('categories:all'), status_code=302)
+        self.assertEqual(len(Note.objects.all()), 1)
+
+
