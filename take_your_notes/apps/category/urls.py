b59ff52203a4b16a35b7a91270be3495ef143c42
admin <magiru@o2.pl>
2020-08-29 00:40:44 +0200

    finished category delete functionality

--- take_your_notes/apps/category/urls.py
+++ take_your_notes/apps/category/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import CategoryCreateView, CategoryListView, CategoryDetailView
+from .views import CategoryCreateView, CategoryListView, CategoryDetailView, CategoryDeleteView
 
 
 urlpatterns = [
     path('create/',CategoryCreateView.as_view(), name='create'),
     path('all/',CategoryListView.as_view(), name='all'),
     path(r'detail/<int:pk>/', CategoryDetailView.as_view(), name='detail'),
+    path(r'delete/<int:pk>/', CategoryDeleteView.as_view(), name='delete'),
 ]
 
--- take_your_notes/apps/category/views.py
+++ take_your_notes/apps/category/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 from django.shortcuts import render
+from django.views.generic.edit import DeleteView
 from django.views.generic import ListView, CreateView, DetailView
 from django.urls import reverse_lazy
 from .models import Category
@@ -34,6 +35,14 @@ class CategoryCreateView(CreateView):
 
 class CategoryDetailView(DetailView):
     model = Category
+
+    @method_decorator(login_required())
+    def dispatch(self, *args, **kwargs):
+        return super().dispatch(*args, **kwargs)
+
+class CategoryDeleteView(DeleteView):
+    model = Category
+    success_url = reverse_lazy('categories:all')
 
     @method_decorator(login_required())
     def dispatch(self, *args, **kwargs):
